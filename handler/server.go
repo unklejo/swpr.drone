a51d7c5fd1b11ced6bc3597c30ef899013e0d761
unkle_jo <jhovie@hypefast.id>
2024-07-03 22:04:51 +0700

    add generated folder with server_interface.go inside

--- handler/server.go
+++ handler/server.go
@@ -1,6 +1,11 @@
 package handler
 
-import "github.com/SawitProRecruitment/UserService/repository"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/unklejo/swpr.drone/repository"
+)
 
 type Server struct {
 	Repository repository.RepositoryInterface
@@ -12,4 +17,14 @@ type NewServerOptions struct {
 
 func NewServer(opts NewServerOptions) *Server {
 	return &Server{}
+}
+
+// type Server struct{}
+
+// func newServer() *Server {
+// 	return &Server{}
+// }
+
+func (s *Server) GetHelloWorld(ctx echo.Context) error {
+	return ctx.String(http.StatusOK, "Hello, World!")
 }
