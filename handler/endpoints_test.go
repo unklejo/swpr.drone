65e2659adfd13516e5343d2a97df2e1848c80b94
unkle_jo <jhovie@hypefast.id>
2024-07-04 17:03:11 +0700

    Add test for CreateEstate

--- handler/endpoints_test.go
+++ handler/endpoints_test.go
@@ -1,7 +1,39 @@
 package handler
 
-import "testing"
+import (
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
 
-func TestGetHello(t *testing.T) {
+	"github.com/golang/mock/gomock"
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+	"github.com/unklejo/swpr.drone/repository/mocks"
+)
 
+func TestCreateEstate(t *testing.T) {
+	// Setup
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodPost, "/estate", strings.NewReader(`{"width":10, "length":10}`))
+	req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockRepo := mocks.NewMockRepositoryInterface(ctrl)
+	h := &Server{
+		Repository: mockRepo,
+	}
+
+	// Expectations
+	mockRepo.EXPECT().CreateEstate(gomock.Any(), 10, 10).Return(nil)
+
+	// Assertions
+	if assert.NoError(t, h.CreateEstate(c)) {
+		assert.Equal(t, http.StatusCreated, rec.Code)
+		assert.Contains(t, rec.Body.String(), "id")
+	}
 }
