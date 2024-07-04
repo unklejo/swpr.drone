e0148519a061da95622643c993d1d2a6887e7694
unkle_jo <jhovie@hypefast.id>
2024-07-04 10:05:00 +0700

    add initial DDL

--- database.sql
+++ database.sql
@@ -8,5 +8,30 @@
 -- This is test table. Remove this table and replace with your own tables. 
 CREATE TABLE test (
 	id serial PRIMARY KEY,
-	name VARCHAR ( 50 ) UNIQUE NOT NULL,
+	name VARCHAR ( 50 ) UNIQUE NOT NULL
 );
+
+-- Table to store estate information
+CREATE TABLE IF NOT EXISTS estates (
+    id UUID PRIMARY KEY,
+    width INTEGER NOT NULL,
+    length INTEGER NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+	updated_at TIMESTAMPTZ DEFAULT NOW()
+);
+
+-- Table to store tree information within estates
+CREATE TABLE IF NOT EXISTS trees (
+    id UUID PRIMARY KEY,
+    estate_id UUID NOT NULL REFERENCES estates(id) ON DELETE CASCADE,
+    x_coordinate INTEGER NOT NULL,
+    y_coordinate INTEGER NOT NULL,
+    height INTEGER NOT NULL,
+    created_at TIMESTAMPTZ DEFAULT NOW(),
+	updated_at TIMESTAMPTZ DEFAULT NOW(),
+    UNIQUE (estate_id, x_coordinate, y_coordinate) -- Ensure no duplicate trees in the same plot
+);
+
+-- Indexes to improve query performance
+CREATE INDEX IF NOT EXISTS idx_trees_estate_id ON trees (estate_id);
+
